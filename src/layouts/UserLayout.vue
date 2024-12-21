7f74b4ac18d6a8dbc8b2192a070b44eacbc0c753
xugx <xugx@dustess.com>
2024-12-21 19:22:45 +0800

    feat: 初始化，删除无用代码

--- src/layouts/UserLayout.vue
+++ src/layouts/UserLayout.vue
@@ -5,7 +5,7 @@
         <div class="top">
           <div class="header">
             <a href="/">
-              <img src="~@/assets/icons/logo.svg" class="logo" alt="logo" />
+              <img src="~@/assets/logo.svg" class="logo" alt="logo" />
               <span class="title">Ant Design</span>
             </a>
           </div>
--- src/router/commonRoutes.ts
+++ src/router/commonRoutes.ts
@@ -9,13 +9,11 @@ import { type Router } from './types'
 // 3.以“/”开头的嵌套路径会被当作根路径，所以子路由上不用加“/”;
 // 在生成路由时，主路由上的path会被自动添加到子路由之前，所以子路由上的path不用在重新声明主路由上的path了。
 export default [
-  // 示例用路由,可删
   {
     path: '/',
     name: 'index',
     component: BasicLayout,
-    meta: { title: 'menu.home' },
-    redirect: '/dashboard',
+    redirect: '/qrCode',
     children: [
       {
         path: '/qrCode',
