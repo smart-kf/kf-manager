b67699af5bb40e77098e58a2a88b5d74da245924
xugx <xugx@dustess.com>
2025-01-02 23:11:22 +0800

    feat:初始化欢迎语和智能欢迎语页面

---
+++ src/components/MaterialDrawer/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <a-modal v-model:open="openModal" :title="title" @ok="handleOk">
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+    <p>Some contents...</p>
+  </a-modal>
+</template>
+
+<script lang="ts" setup>
+import { ref, defineProps, onMounted, computed } from 'vue'
+import { defineEmits } from 'vue'
+
+const emits = defineEmits(['update:modelValue'])
+
+const props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: true
+  },
+  title: {
+    type: String,
+    default: '添加欢迎语'
+  },
+  isAi: {
+    // 是否是智能欢迎语
+    type: Boolean,
+    default: false
+  },
+  actionType: {
+    type: String,
+    default: 'add' // edit
+  },
+  editData: {
+    type: Object,
+    default: () => {}
+  }
+})
+const openModal = computed({
+  get: () => {
+    return props.modelValue
+  },
+  set: (val) => {
+    emits('update:modelValue', val)
+  }
+})
+
+const handleOk = () => {}
+
+// 初始化信息，数据回显
+const initData = () => {}
+onMounted(() => {
+  initData()
+})
+</script>
+
+<style lang="less" scoped></style>
--- src/views/welcomeMsg/index.vue
+++ src/views/welcomeMsg/index.vue
@@ -1,9 +1,169 @@
 <!-- 二维码页面 -->
 <template>
-  <div class="kf-body">欢迎语{{ title }}</div>
+  <div class="kf-body">
+    <div class="top-action">
+      <a-button type="primary" @click="onAddMsg">添加欢迎语</a-button>
+      <div @click="onRefesh">
+        <span class="refesh">
+          <ReloadOutlined />
+        </span>
+      </div>
+    </div>
+    <a-table bordered :data-source="state.dataSource" :columns="columns" size="middle">
+      <template #bodyCell="{ column, text, record }">
+        <template v-if="column.dataIndex === 'content'">
+          <template v-if="record.contentType === 'video'">
+            <span>{{ record.content }}</span>
+          </template>
+          <template v-else-if="record.contentType === 'image'">
+            <a-image :width="100" :height="100" :src="record.content" :fallback="failImg" />
+          </template>
+          <template v-else>
+            <span>{{ record.content }}</span>
+          </template>
+        </template>
+        <template v-if="column.dataIndex === 'sendSort'">
+          <a-input-number id="inputNumber" v-model:value="record.sendSort" :min="1" :max="9999" />
+        </template>
+        <template v-if="column.dataIndex === 'status'">
+          <a-switch v-model:checked="record.status" />
+        </template>
+
+        <template v-if="column.dataIndex === 'operation'">
+          <a-space>
+            <span class="table-link-action" @click="onEdit(record)"> <EditOutlined />修改 </span>
+
+            <a-popconfirm title="确定要删除吗？" @confirm="onDelete(record)">
+              <span class="table-link-action"> <DeleteOutlined />删除 </span>
+            </a-popconfirm>
+          </a-space>
+        </template>
+      </template>
+    </a-table>
+    <MaterialDrawer v-model:model-value="state.showDia"></MaterialDrawer>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { computed, ref } from 'vue'
-const title = ref('test')
+import { computed, ref, onMounted, reactive } from 'vue'
+import { h } from 'vue'
+import { ReloadOutlined, EditOutlined, DeleteOutlined } from '@ant-design/icons-vue'
+import MaterialDrawer from '@/components/MaterialDrawer/index.vue'
+import { message } from 'ant-design-vue'
+import logo from '@/assets/defaultUser.png'
+import failImg from '@/assets/failImg.png'
+
+const state = reactive({
+  dataSource: [],
+  editableData: {},
+  editData: {},
+  actionType: 'add',
+  showDia: false
+})
+
+const columns = [
+  {
+    title: '回复内容',
+    key: 'content',
+    align: 'center',
+    dataIndex: 'content'
+  },
+  {
+    title: '发送顺序(小到大)',
+    key: 'sendSort',
+    align: 'center',
+    dataIndex: 'sendSort',
+    width: 200
+  },
+  {
+    title: '是否启用',
+    align: 'center',
+    dataIndex: 'status',
+    key: 'status',
+    width: 200
+  },
+  {
+    title: '操作',
+    align: 'center',
+    dataIndex: 'operation',
+    width: 200
+  }
+]
+
+const onRefesh = () => {
+  console.log(222)
+}
+// 新增欢迎语
+const onAddMsg = () => {
+  state.actionType = 'add'
+  state.showDia = true
+}
+const onEdit = (item) => {
+  state.actionType = 'edit'
+  state.editData = item
+}
+
+const onDelete = (item) => {
+  state.dataSource = state.dataSource.filter((el) => el.id !== item.id)
+  state.showDia = true
+}
+
+const initData = () => {
+  state.dataSource = [
+    {
+      id: 1,
+      content: '文本',
+      sendSort: 1,
+      contentType: 'text',
+      status: true,
+      updateTime: '2024-13-13 13:13:13'
+    },
+    {
+      id: 2,
+      content: logo,
+      sendSort: 2,
+      contentType: 'image',
+      status: true,
+      updateTime: '2024-13-13 13:13:13'
+    },
+    {
+      id: 3,
+      content: '视频',
+      sendSort: 3,
+      contentType: 'video',
+      status: false,
+      updateTime: '2024-13-13 13:13:13'
+    }
+  ]
+  console.log(state)
+}
+
+onMounted(() => {
+  initData()
+})
 </script>
+<style lang="less" scoped>
+.top-action {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding-bottom: 12px;
+}
+.refesh {
+  cursor: pointer;
+  width: 27px;
+  height: 27px;
+  line-height: 24px;
+  text-align: center;
+  display: inline-block;
+  border-radius: 50%;
+  border: 1px solid #eee;
+  background-color: #fff;
+  &:hover {
+    background-color: #eee;
+  }
+}
+.anticon-edit span {
+  margin-inline-start: 0;
+}
+</style>
