b9e1b2ab5649d12542799f158feadf58cc85e359
tianzhuling <gotojoy@163.com>
2025-01-09 00:07:54 +0800

    feat: 路由hash

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,11 @@
 import config from '@/config/defaultSettings';
-import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router';
+import { RouteRecordRaw, createRouter, createWebHashHistory } from 'vue-router';
 import { setupBeforeEach, setupAfterEach } from './routerGuard'
 import generateAsyncRoutes from './generateAsyncRoutes'
 import routes from './commonRoutes'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routes as unknown as RouteRecordRaw[],
 });
 
